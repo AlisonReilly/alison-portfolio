a8782b13ffa3f6fd0a71fbd43c688d1fce6691e8
Alison Reilly <reillyamr@gmail.com>
2023-10-27 14:07:53 -0400

    added mobile outside click

--- src/Components/Navigation/MobileNavUL.tsx
+++ src/Components/Navigation/MobileNavUL.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { MainNavItems, NavItem } from '../../constants/dataConstants';
 import '../../Styles/MobileNav.css';
 import { MobileNavItem } from './MoblieNavLI';
@@ -11,10 +11,24 @@ type MobileNavProps = {
 export const MobileNav: React.FC<MobileNavProps & React.HTMLProps<HTMLDivElement>> = ({
     menuItemData, text, ...htmlProps}) => { 
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const ButtonRef = createRef<HTMLElement>();
+
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            if (ButtonRef.current && !ButtonRef.current.contains(event.target)) {
+            setIsOpen(false)
+            }
+        }
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+
+    }, [ButtonRef])
     
     return (
-        <nav id='PrimaryMobileNav' className='mobile-nav' role="navigation" aria-label="Main menu">
-            <button id='mobile-nav-button' aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}/>
+        <nav ref={ButtonRef} id='PrimaryMobileNav' className='mobile-nav' role="navigation" aria-label="Main menu">
+            <button  id='mobile-nav-button' aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}/>
             <ul className={`flexcolumn glass mobile-ul ${!isOpen ? 'closed-menu' : ''}`}>
             {isOpen && MainNavItems.map((i, k) =>
                 <MobileNavItem navHref={i.href} navName={i.name} navSubItems={i.subNav} isOpen={isOpen} setIsOpen={setIsOpen}/>
