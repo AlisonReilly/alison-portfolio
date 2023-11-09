aff21d53c97b69aca9a2bbb235916927f3e8ac6e
Alison Reilly <reillyamr@gmail.com>
2023-11-08 22:11:27 -0500

    added action buttons

--- src/Containers/HeroSection.tsx
+++ src/Containers/HeroSection.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { TextFade } from '../Components/TextEffects/TextFade';
 import '../Styles/Hero.css';
 import { BasicDiagram } from '../Components/Diagramming/BasicDiagram';
+import { Link } from 'react-router-dom';
   
 
 type HeroProps = {
@@ -17,7 +18,8 @@ export const HeroSection: React.FC<HeroProps & React.HTMLProps<HTMLDivElement>> 
     const [welcomeTimingMet, setWelcomeTimingMet] = useState<boolean>(false);
     const [skillsTimingMet, setSkillsTimingMet] = useState<boolean>(false);
     const [titleTimingMet, setTitleTimingMet] = useState<boolean>(false);
-
+    const [actionTimingMet, setActionTimingMet] = useState<boolean>(false);
+ 
     useEffect(() => {
         setTimeout(() => {
             setWelcomeTimingMet(true)
@@ -28,36 +30,41 @@ export const HeroSection: React.FC<HeroProps & React.HTMLProps<HTMLDivElement>> 
         setTimeout(() => {
             setTitleTimingMet(true)
         }, 9000)
+        setTimeout(() => {
+            setActionTimingMet(true)
+        }, 12000)
     }, [])
     
     return (
         <div id='Hero'>
-        {/* display the welcome timing, once seconds met display the name */}
-        {welcomeTimingMet 
-            ? 
-            <div>
-            <BasicDiagram figureNumber='2'/>
-            <TextFade 
-                classNames='fade-in-only main-text diagram-outline' 
-                text='Alison Reilly'/>
+            {/* display the welcome timing, once seconds met display the name */}
+            {welcomeTimingMet 
+                ? 
+                <div>
+                    <BasicDiagram figureNumber='2'/>
+                    <TextFade 
+                        classNames='fade-in-only main-text diagram-outline' 
+                        text='Alison Reilly'/>
+                </div>
+                :
+                <TextFade
+                    classNames='fade-in-out-text main-text'
+                    text='Welcome'/>
 
-  
-            </div>
-            :
-            <TextFade
-                classNames='fade-in-out-text main-text'
-                text='Welcome'/>
-
-        }
-        {/* display the skills interlude, switch to title after */}
-        {skillsTimingMet ? titleTimingMet ?
-            <TextFade
-                classNames='fade-in-only support-text support-text-adjust'
-                text='Full Stack Software Engineer'/> :
-            <TextFade
-                classNames='fade-in-out-text support-text support-text-adjust'
-                text='React TypeScript Python'/> : null
-        }
+            }
+            {/* display the skills interlude, switch to title after */}
+            {skillsTimingMet ? titleTimingMet ?
+                <TextFade
+                    classNames='fade-in-only support-text support-text-adjust'
+                    text='Full Stack Software Engineer'/> :
+                <TextFade
+                    classNames='fade-in-out-text support-text support-text-adjust'
+                    text='React TypeScript Python'/> : null
+            }
+            {actionTimingMet ? <div className='action-buttons'>
+                <Link className='modern-link action-button' to={'/resume'}>View Experience</Link>
+                <Link className='modern-link action-button' to={'/projects'}>View Foundations</Link>
+            </div> : null }
         </div>
     ); 
 } 
