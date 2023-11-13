2cc9897c07531ef9c66ce68cdb4af7bc8eb82a00
Alison Reilly <reillyamr@gmail.com>
2023-11-13 08:45:27 -0500

    game ready adjustments

--- src/Containers/GameDemo.tsx
+++ src/Containers/GameDemo.tsx
@@ -13,21 +13,12 @@ export const GameDemo: React.FC<GameDemoProps & React.HTMLProps<HTMLDivElement>>
         const [currentGame, setCurrentGame] = useState<Phaser.Game>();
         const [_pointerType, setPointerType] = useState<string>(pointerType || 'mouse');
         const [isLandscape, setIsLandscape] = useState<boolean>(window.innerWidth > window.innerHeight);
-        
-        console.log('navigator: ', navigator.userAgent.toLowerCase().includes('mobile'))
+        const [gameReady, setGameReady] = useState<boolean>(window.innerWidth > window.innerHeight);
 
         let isIPhone = navigator.userAgent.toLowerCase().includes('iphone');
-        console.log('pointer type: ', pointerType === 'mouse')
+
         let width = _pointerType === 'mouse' || !navigator.userAgent.toLowerCase().includes('mobile') ? 800 : window.innerWidth < window.innerHeight ? window.innerHeight : window.innerWidth;
         let height = _pointerType === 'mouse' || !navigator.userAgent.toLowerCase().includes('mobile') ? 600 : window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth;
-        let scaleValues;
-        // if (_pointerType === 'touch' || navigator.userAgent.toLowerCase().includes('mobile')){
-        //     scaleValues = {
-        //         mode: Phaser.Scale.FIT,
-        //         autoCenter: Phaser.Scale.CENTER_BOTH
-        //     }
-        // }
-        
 
         let gameConfig = {
             type: Phaser.AUTO, 
@@ -51,7 +42,8 @@ export const GameDemo: React.FC<GameDemoProps & React.HTMLProps<HTMLDivElement>>
 
         useEffect(() => {
             const handleResize = () => {
-              setIsLandscape(window.innerWidth > window.innerHeight)
+                setIsLandscape(window.innerWidth > window.innerHeight)
+                setGameReady(window.innerWidth > window.innerHeight)
             }
         
             window.addEventListener('resize', handleResize)
@@ -60,6 +52,7 @@ export const GameDemo: React.FC<GameDemoProps & React.HTMLProps<HTMLDivElement>>
             };
         }, [])
 
+        // todo, userAgent check for mobile in string seems to be sufficient so may not need this, same in other components
         useEffect(() => {
             const checkPointerType = (event: any) => {
                 if (event.pointerType === "mouse") { setPointerType('mouse')}
@@ -73,9 +66,12 @@ export const GameDemo: React.FC<GameDemoProps & React.HTMLProps<HTMLDivElement>>
         }, []);
 
         useEffect(() => {
-            if (!currentGame) {
-                setCurrentGame(new Phaser.Game(gameConfig))
+            let game: Phaser.Game;
+            if (!currentGame && gameReady) {
+                game = new Phaser.Game(gameConfig)
+                setCurrentGame(game)
             }
+
         }, [currentGame]);
     
 
@@ -83,7 +79,8 @@ export const GameDemo: React.FC<GameDemoProps & React.HTMLProps<HTMLDivElement>>
         !isLandscape && (_pointerType === 'touch' || navigator.userAgent.toLowerCase().includes('mobile'))
         ? 
         <div className='rotate-message'>
-            Rotate your screen to landscape/horizontal
+            <div>Rotate your screen to landscape/horizontal</div>
+            
         </div> 
         : 
         <div id='canvas' className={`${_pointerType === 'touch' || navigator.userAgent.toLowerCase().includes('mobile') ? 'canvas-mobile' : 'canvas-desktop'}`}/>
