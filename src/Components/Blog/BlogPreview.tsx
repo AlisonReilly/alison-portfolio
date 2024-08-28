a055344d9cb277a13735c8196e4e4aba908e18f9
Alison Reilly <reillyamr@gmail.com>
2024-08-28 12:38:30 -0400

    added vite, removed react-scripts

--- src/Components/Blog/BlogPreview.tsx
+++ src/Components/Blog/BlogPreview.tsx
@@ -15,7 +15,6 @@ interface BlogPreviewProps {
 
 export const BlogPreview: React.FC<BlogPreviewProps & React.HTMLProps<HTMLDivElement>> = (
     { blogURL, image, blogContent, title, date, tags, altText }) => {
-        console.log('date: ', typeof date)
     return (
         <div id='BlogPreview' className='blogPreviewPane'>
             <h2 className='blog-preview-title'>{title}</h2>
